46a72c26d0b6a3dacafb78c382fd16b872837020
setocourtney <courtneyjseto@gmail.com>
2020-03-22 16:36:15 -0700

    removed static directory

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.resolve('./public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
