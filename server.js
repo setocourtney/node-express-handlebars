4178675fc6445b2e6c42d7c26c7eedea2ff6c324
setocourtney <courtneyjseto@gmail.com>
2020-03-22 01:05:24 -0700

    trying procfile again

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve('./public')));
 app.use(express.urlencoded({ extended: true }));
