3c7dad03420af0286de66a8a096c5db27244a9fd
setocourtney <courtneyjseto@gmail.com>
2020-03-22 17:29:10 -0700

    fixed img src

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
