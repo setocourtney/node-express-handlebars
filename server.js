5bba610c51d9614a5e40edc224d7957429d748a4
setocourtney <courtneyjseto@gmail.com>
2020-03-22 17:31:29 -0700

    img src

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("/public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
